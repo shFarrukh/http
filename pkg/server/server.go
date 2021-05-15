aa0f296c7469b6fb355e14bb5435a03075188944
Farrukh <sh.farrukh99@mail.ru>
2021-05-15 18:44:52 +0300

    Fixed 22th solution

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,28 +1,37 @@
 package server
 
 import (
-	"bytes"
+	"net/url"
+	
 	"io"
 	"log"
-	"net"
+	"bytes"
 	"strings"
 	"sync"
+	"net"
 )
 
-type HandlerFunc func(conn net.Conn)
+type HandlerFunc func(req *Request) 
+
 type Server struct {
-	addr string 
+	addr string
 	mu sync.RWMutex
 	handlers map[string]HandlerFunc
-
 }
 
-func NewServer(addr string) *Server{
+type Request struct {
+	Conn net.Conn
+	QueryParams url.Values
+	PathParams map[string]string
+	Headers map[string]string
+	Body []byte
+}
+
+func NewServer(addr string) *Server {
 	return &Server{addr: addr, handlers: make(map[string]HandlerFunc)}
-
 }
 
-func (s *Server) Register (path string, handler HandlerFunc) {
+func (s *Server) Register(path string, handler HandlerFunc) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.handlers[path] = handler
@@ -52,52 +61,154 @@ func (s *Server) Start() (err error) {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-
+  
 	buf := make([]byte, 4096)
 	for {
-		n, err := conn.Read(buf)
-		if err == io.EOF {
-			log.Printf("%s", buf[:n])
-		}
-		if err != nil {
-			log.Println(err)
-			return
-		}
+	  n, err := conn.Read(buf)
 
-		data := buf[:n]
-		rLD := []byte{'\r', '\n'}
-		rLE := bytes.Index(data, rLD)
-		if rLE == -1 {
-			log.Println("ErrBadRequest")
-			return
-		}
+	  if err == io.EOF {
+		log.Printf("%s", buf[:n])
+	  }
+	  if err != nil {
+		log.Println(err)
+		return
+	  }
+	  
+	  var req Request
+	  data := buf[:n]
+	  rLD := []byte{'\r', '\n'}
+	  rLE := bytes.Index(data, rLD)
+	  if rLE == -1 {
+		log.Println("ErrBadRequest")
+		return
+	  }
+	  
+	  hld:=[]byte{'\r', '\n', '\r', '\n'}
+	  hLE := bytes.Index(data, hld)
+	  if rLE == -1 {
+		return
+	  }
 
-		reqLine := string(data[:rLE])
-		parts := strings.Split(reqLine, " ")
+	  headersLine := string(data[rLE:hLE])
+	  headers := strings.Split(headersLine, "\r\n")[1:]
 
-		if len(parts) != 3 {
-			log.Println("ErrBadRequest")
-			return
-		}
+	  mp := make(map[string]string)
+	  for _, v := range headers {
+		headerLine := strings.Split(v, ": ")
+		mp[headerLine[0]] = headerLine[1]
+	  }
 
-		path, version := parts[1], parts[2]
+	  req.Headers = mp
 
-		if version != "HTTP/1.1" {
-			log.Println("ErrHTTPVersionNotValid")
-			return
-		}
+	  req.Body=data[hLE+4:]
 
-		var handler = func(conn net.Conn) {
-			conn.Close()
-		}
-		s.mu.RLock()
-		for i := 0; i < len(s.handlers); i++ {
-			if hr, found := s.handlers[path]; found {
-				handler = hr
-				break
-			}
-		}
-		s.mu.RUnlock()
-		handler(conn)
-	}
+	  reqLine := string(data[:rLE])
+	  parts := strings.Split(reqLine, " ")
+  
+	  if len(parts) != 3 {
+		log.Println("ErrBadRequest")
+		return
+	  }
+  
+	  path, version := parts[1], parts[2]
+      
+	  if version != "HTTP/1.1" {
+		log.Println("ErrHTTPVersionNotValid")
+		return
+	  }
+	  decoded, err:=url.PathUnescape(path)
+	  if err!=nil {
+		  log.Println(err)
+		  return
+	  }
+	  log.Println(decoded)
+
+	  uri, err:=url.ParseRequestURI(decoded)
+	  if err!=nil {
+		log.Println(err)
+		return
+	  }
+	  
+	  req.Conn = conn
+	  req.QueryParams = uri.Query()
+	//   req.PathParams = uri.Path
+	//   log.Print(uri.Path)
+	//   log.Print(uri.Query())
+	  
+	  
+	  var handler = func(req *Request) {
+		req.Conn.Close()
+	  }
+	  s.mu.RLock()
+	//   pathP, err :=s.checkPath(uri.Path)
+	//   if err!=nil {
+	// 	req.PathParams = pathP
+	//   }
+      
+	  
+	//   for i := 0; i < len(s.handlers); i++ {
+	// 	if hr, found := s.handlers[uri.Path]; found {
+	// 	  handler = hr
+	// 	  break
+	// 	}
+	//   }
+	  pathParam, hr:=s.checkPath(uri.Path)
+	  if hr!=nil {
+		req.PathParams = pathParam
+		handler = hr
+	  }
+	  s.mu.RUnlock()
+	  handler(&req) 
+	} 
 }
+
+func (s *Server) checkPath(path string) (map[string]string, HandlerFunc) {
+
+	strRoutes := make([]string, len(s.handlers))
+	i := 0
+	for k := range s.handlers {
+	  strRoutes[i] = k
+	  i++
+	}
+  
+	mp := make(map[string]string)
+    
+	for i := 0; i < len(strRoutes); i++ {
+	  flag := false
+	  route := strRoutes[i]
+	  partsRoute := strings.Split(route, "/")
+	  pRotes := strings.Split(path, "/")
+      
+	  for j, v := range partsRoute {
+		if v != "" {
+		  f := v[0:1]
+		  l := v[len(v)-1:]
+		  if f == "{" && l == "}" {
+			mp[v[1:len(v)-1]] = pRotes[j]
+			flag = true
+		  } else if pRotes[j] != v {
+  
+			strs := strings.Split(v, "{")
+			if len(strs) > 0 {
+			  key := strs[1][:len(strs[1])-1]
+			  mp[key] = pRotes[j][len(strs[0]):]
+			  flag = true
+			} else {
+			  flag = false
+			  break
+			}
+		  }
+		  flag = true
+		}
+	  }
+	  if flag {
+		if hr, found := s.handlers[route]; found {
+		  return mp, hr
+		}
+		break
+	  }
+	}
+  
+	return nil, nil
+  
+  }
